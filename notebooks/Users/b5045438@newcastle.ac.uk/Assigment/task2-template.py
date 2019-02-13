6fb88692520a1eb8e1a4d2ae16b4d2b66b6484f6
Ammar Hasan <ammar199733@gmail.com>
2019-02-13 13:34:15 +0000

    Minor refactoring

--- notebooks/Users/b5045438@newcastle.ac.uk/Assigment/task2-template.py
+++ notebooks/Users/b5045438@newcastle.ac.uk/Assigment/task2-template.py
@@ -8,6 +8,12 @@
 
 # COMMAND ----------
 
+# MAGIC %md ## Library and Data Loading
+# MAGIC 
+# MAGIC Load appropriate libraries and ratings file. Also create constant for edges file location.
+
+# COMMAND ----------
+
 import pandas as pd
 import pyspark.sql.functions as f
 from pyspark.sql.types import *
@@ -17,10 +23,10 @@ from collections import Counter
 
 # COMMAND ----------
 
-# IN
+# IN File locations
 RATINGS_SMALL_PARQUET = "/FileStore/tables/ratings-small.parquet"
 
-# OUT
+# OUT file locations
 EDGES_SMALL_PARQUET = "/FileStore/tables/150454388/edges-small.parquet"
 
 # COMMAND ----------
@@ -42,7 +48,7 @@ ratings.agg(f.countDistinct('userId')).show()
 
 # COMMAND ----------
 
-# MAGIC %md ### Finding Edges
+# MAGIC %md ## Finding Edges
 
 # COMMAND ----------
 
@@ -62,7 +68,9 @@ display(ratings)
 
 # COMMAND ----------
 
-# MAGIC %md #### Dropping Ratings and Timestamps
+# MAGIC %md ### Dropping Ratings and Timestamps
+# MAGIC 
+# MAGIC These two fields are not required since the edges will just record the nodes between the edges and weighting.
 
 # COMMAND ----------
 
@@ -71,7 +79,9 @@ ratings = ratings.drop('rating')
 
 # COMMAND ----------
 
-# MAGIC %md #### Create Dataframes
+# MAGIC %md ### Create Dataframes
+# MAGIC 
+# MAGIC Two dataframes will be used to help divide the task up, each representing one of the user ids in the edge
 
 # COMMAND ----------
 
@@ -88,7 +98,9 @@ df1.count()
 
 # COMMAND ----------
 
-# MAGIC %md #### Renaming Rows 
+# MAGIC %md ### Renaming Rows 
+# MAGIC 
+# MAGIC Rename userId rows before joining the two dataframes to represent the first user id (userId1) and the second user id (userId2) in the edges.
 
 # COMMAND ----------
 
@@ -105,7 +117,9 @@ display(df2)
 
 # COMMAND ----------
 
-# MAGIC %md #### Joining Second Dataframe with RDD
+# MAGIC %md ### Joining Second Dataframe with RDD
+# MAGIC 
+# MAGIC Using an outer join, connect the two dataframes based on movieId to connect users that rated the same movie. Make sure to restrict direction to avoid duplicates.
 
 # COMMAND ----------
 
@@ -117,7 +131,7 @@ j_df.count()
 
 # COMMAND ----------
 
-# j_df = j_df[~(j_df['userId1'] == j_df['userId2'])]
+# restrict direction
 j_df = j_df[(j_df['userId1'] < j_df['userId2'])]
 
 # COMMAND ----------
@@ -130,7 +144,9 @@ j_df.count()
 
 # COMMAND ----------
 
-# MAGIC %md #### Finding Weights 
+# MAGIC %md ### Finding Weights 
+# MAGIC 
+# MAGIC Now that we have all pairs of users that watch the same movie, we can group by the ids and count the occurances to get the weights (number of shared movie ratings).
 
 # COMMAND ----------
 
@@ -139,6 +155,10 @@ weightedEdges= j_df.groupby(df1['userId1'], df2['userId2']).count()
 # COMMAND ----------
 
 weightedEdges.count()
+
+# COMMAND ----------
+
+# MAGIC %md ### Save Weights
 
 # COMMAND ----------
 
